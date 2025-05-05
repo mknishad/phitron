946fe3c3495c39c0aa149ae655179c88b102e724
Monir <mknishad1@ymail.com>
2025-05-06 00:09:31 +0600

    Add module 14.5

---
+++ Introduction to Programming Language/Module 14.5/W_Shape_3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int star = 1;
+    int space = n - 1;
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= star; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+
+        star += 2;
+        space--;
+    }
+
+    star -= 2;
+    space = 0;
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= star; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+
+        star -= 2;
+        space++;
+    }
+
+    return 0;
+}
