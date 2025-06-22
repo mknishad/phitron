aef195f7fdbe4a14966812698e2b7be86a77529b
Monir <mknishad1@ymail.com>
2025-06-22 22:49:50 +0600

    Add module 3

---
+++ Basic Data Structures/Module 3/Z_Binary_Search.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    while (q--)
+    {
+        int x;
+        cin >> x;
+
+        int l = 0, r = n - 1;
+        bool found = false;
+
+        while (l <= r)
+        {
+            int mid = (l + r) / 2;
+
+            if (a[mid] == x)
+            {
+                found = true;
+                break;
+            }
+            else if (a[mid] < x)
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+
+        if (found)
+        {
+            cout << "found" << endl;
+        }
+        else
+        {
+            cout << "not found" << endl;
+        }
+    }
+
+    return 0;
+}
