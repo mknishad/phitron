ff050c5818ed90f02f5f2019956e1c46eb39332f
Monir <mknishad1@ymail.com>
2025-06-29 12:57:01 +0600

    Add module 5

---
+++ Basic Data Structures/Module 5/Printing_linked_list.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    Node* head = new Node(1);
+    Node* a = new Node(2);
+    Node* b = new Node(3);
+    Node* c = new Node(4);
+
+    head->next = a;
+    a->next = b;
+    b->next = c;
+
+    Node* curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->val << endl;
+        curr = curr->next;
+    }
+
+    return 0;
+}
