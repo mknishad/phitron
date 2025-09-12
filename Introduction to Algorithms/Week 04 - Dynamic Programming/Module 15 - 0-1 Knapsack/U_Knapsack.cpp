7355179305d9388847897328887c4743fbd4744c
Monir <mknishad1@ymail.com>
2025-09-12 11:50:13 +0600

    Module 15: 0-1 Knapsack:

---
+++ Introduction to Algorithms/Week 04 - Dynamic Programming/Module 15 - 0-1 Knapsack/U_Knapsack.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int weight[25];
+int value[25];
+int dp[25][105];
+
+int knapsack(int i, int max_weight)
+{
+    if (i < 0 || max_weight <= 0)
+        return 0;
+
+    if (dp[i][max_weight] != -1)
+        return dp[i][max_weight];
+    
+    if (weight[i] <= max_weight)
+    {
+        int opt1 = knapsack(i - 1, max_weight - weight[i]) + value[i];
+        int opt2 = knapsack(i - 1, max_weight);
+        dp[i][max_weight] = max(opt1, opt2);
+        return dp[i][max_weight];
+    }
+    else
+    {
+        dp[i][max_weight] = knapsack(i - 1, max_weight);
+        return dp[i][max_weight];
+    }
+}
+
+int main()
+{
+    int n, max_weight;
+    cin >> n >> max_weight;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> weight[i] >> value[i];
+    }
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j <= max_weight; j++)
+            dp[i][j] = -1;
+    
+    cout << knapsack(n - 1, max_weight) << endl;
+
+    return 0;
+}
